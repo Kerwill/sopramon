3e90024ebbd85232d039f501f6148f479680bb59
Unknown <guillaume.dreno@gmail.com>
2018-08-02 16:36:25 +0200

    stash

--- sopramon-java/src/main/java/fr/sopra/DAOHibernate/PrincipalHibernate.java
+++ sopramon-java/src/main/java/fr/sopra/DAOHibernate/PrincipalHibernate.java
@@ -1,16 +1,19 @@
 package fr.sopra.DAOHibernate;
 
 import fr.sopra.DAO.IDAOBoss;
+import fr.sopra.DAO.IDAOCoup;
 import fr.sopra.DAO.IDAOSopramon;
 import fr.sopra.model.game.Boss;
+import fr.sopra.model.game.Coup;
 import fr.sopra.model.game.Sopramon;
 
 public class PrincipalHibernate {
 
 	public static void main(String[] args) {
 		
-		printSopramons();
+//		printSopramons();
 //		printBoss();
+		printCoup();
 	}
 	public static void printSopramons() {
 		IDAOSopramon daoSopramon = new DAOSopramonHibernate();
@@ -25,6 +28,14 @@ public class PrincipalHibernate {
 		
 		for (Boss  b : daoBoss .findAll()) {
 			System.out.println(b.getNom());
+		}
+	}
+	
+	public static void printCoup() {
+		IDAOCoup daoCoup  = new DAOCoupHibernate();
+		
+		for (Coup  c : daoCoup.findAll()) {
+			System.out.println(c.getDegats());
 		}
 	}
 }
