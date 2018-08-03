8735dab60c8072c3ab4201c7b646de65fd0c95da
Unknown <aastruc86@gmail.com>
2018-08-03 17:08:09 +0200

    vdvddv

--- sopramon-java/src/main/java/fr/sopra/DAOHibernate/ProgrammeGenerator.java
+++ sopramon-java/src/main/java/fr/sopra/DAOHibernate/ProgrammeGenerator.java
@@ -2,35 +2,71 @@ package fr.sopra.DAOHibernate;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
-
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
-import javax.validation.constraints.Size;
-
-import fr.sopra.DAO.IDAOBoss;
-import fr.sopra.DAO.IDAOItem;
-import fr.sopra.DAO.IDAOSopramon;
-import fr.sopra.model.game.Achat;
 import fr.sopra.model.game.Boss;
-import fr.sopra.model.game.Capacite;
-import fr.sopra.model.game.Coup;
-import fr.sopra.model.game.Item;
-import fr.sopra.model.game.Signe;
 import fr.sopra.model.game.Sopramon;
-import fr.sopra.model.game.Type;
 
 public class ProgrammeGenerator {
 
 	public static void main(String[] args) throws ParseException {
-		
+
+	}
+
+	public static Sopramon addSopramon() throws ParseException {
+
 		Scanner keyboard = new Scanner(System.in);
-		
-//cr�er un boss
-		System.out.println("-----Cr�ation d'un boss----");
+		DAOSopramonHibernate daoSopramon = new DAOSopramonHibernate();
+		Sopramon mySopramon = new Sopramon();
+
+		System.out.println("-----Creation du compte----");
+		System.out.println("Saisir votre prenom");
+		String prenomUtil = keyboard.next();
+
+		System.out.println("Saisir votre nom :");
+		String nomUtil = keyboard.next();
+
+		System.out.println("Saisir votre mot de passe :");
+		String motDePasse = keyboard.next();
+
+		System.out.println("Entrez votre nom d'utilisateur");
+		String username = keyboard.next();
+
+		System.out.println("Entrez le nom de votre Sopramon");
+		String nomSopra = keyboard.next();
+
+		System.out.println("Veuillez saisir votre jour de naissance");
+		int jourNaissance = keyboard.nextInt();
+		System.out.println("Veuillez saisir votre mois de naissance");
+		int moisNaissance = keyboard.nextInt();
+		System.out.println("Veuillez saisir votre annee de naissance");
+		int anneeNaissance = keyboard.nextInt();
+
+		System.out.println(jourNaissance + " " + moisNaissance + " " + anneeNaissance);
+
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+		mySopramon.setNomUtil(nomUtil);
+		mySopramon.setPrenomUtil(prenomUtil);
+		mySopramon.setUsername(username);
+		mySopramon.setPassword(motDePasse);
+		mySopramon.setDateNaissance(formatter.parse(jourNaissance + "/" + moisNaissance + "/" + anneeNaissance));
+		mySopramon.setExperience(0);
+		mySopramon.setNiveau(1);
+		mySopramon.setArgent(100.00d);
+		mySopramon.setNom(nomSopra);
+		mySopramon.setSigne(SigneGenerator
+				.getAstrologicalSign(formatter.parse(jourNaissance + "/" + moisNaissance + "/" + anneeNaissance)));
+		daoSopramon.save(mySopramon);
+		keyboard.close();
+		DAOSopramonHibernate.close();
+		return mySopramon;
+	}
+
+	public static void addBoss() throws ParseException {
+
+		DAOBossHibernate daoBoss = new DAOBossHibernate();
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("-----Creation d'un boss----");
 		System.out.println("Entrez le nom du boss : ");
 		String nomBoss = keyboard.next();
 		System.out.println("Entrez le niveau du boss : ");
@@ -45,186 +81,33 @@ public class ProgrammeGenerator {
 		int esquiveBoss = keyboard.nextInt();
 		System.out.println("Entrez la vitesse du boss : ");
 		int vitesseBoss = keyboard.nextInt();
-		
-		 System.out.println("Veuillez saisir le jour de naissance");
-		 int jourNaissance = keyboard.nextInt();
-		 System.out.println("Veuillez saisir le mois de naissance");
-		 int moisNaissance = keyboard.nextInt();
-		 System.out.println("Veuillez saisir l'annee de naissance");
-		 int anneeNaissance = keyboard.nextInt();
 
-		 System.out.println(jourNaissance+" "+moisNaissance+" "+anneeNaissance);
-		 
-		 SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-		
-		addBoss(nomBoss, niveauBoss,  attaqueBoss, defenseBoss, pdvBoss, 
-				esquiveBoss, vitesseBoss, formatter.parse(jourNaissance + "/" + moisNaissance + "/" + anneeNaissance));
-		
+		System.out.println("Veuillez saisir le jour de naissance");
+		int jourNaissance = keyboard.nextInt();
+		System.out.println("Veuillez saisir le mois de naissance");
+		int moisNaissance = keyboard.nextInt();
+		System.out.println("Veuillez saisir l'annee de naissance");
+		int anneeNaissance = keyboard.nextInt();
+
+		System.out.println(jourNaissance + " " + moisNaissance + " " + anneeNaissance);
+
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
 		keyboard.close();
-		
-//cr�er un item		
-		System.out.println("-----Cr�ation d'un item ----");
-		System.out.println("Entrez le nom de l'item : ");
-		String nomItem = keyboard.next();
-		System.out.println("Entrez le prix de l'item : ");
-		double prixItem = keyboard.nextDouble();
-		System.out.println("Entrez l'attaque de l'item : ");
-		int attaqueItem = keyboard.nextInt();
-		System.out.println("Entrez la defense de l'item : ");
-		int defenseItem = keyboard.nextInt();
-		System.out.println("Entrez les pdv de l'item : ");
-		int pdvItem = keyboard.nextInt();
-		System.out.println("Entrez l'esquive de l'item : ");
-		int esquiveItem = keyboard.nextInt();
-		System.out.println("Entrez la vitesse de l'item : ");
-		int vitesseItem = keyboard.nextInt();
-		
-		addItem(nomItem, prixItem, attaqueItem, defenseItem, pdvItem, esquiveItem, vitesseItem); // nom / prix / attaque / defense / pdv / esquive / vitesse
-
-		
 		DAOHibernate.close();
-	}   
-
-	public static Sopramon addSopramon(@Size(max = 30) @NotEmpty String nomUtil,
-			@Size(max = 30) @NotEmpty String prenomUtil, @Size(max = 30) @NotEmpty String username,
-			@Size(max = 30) @NotEmpty String password, @NotEmpty Date dateNaissance,
-			@NotEmpty @Size(max = 30) String nom) {
-		
-		DAOSopramonHibernate daoSopramon = new DAOSopramonHibernate();
-		Sopramon mySopramon = new Sopramon();
-
-		mySopramon.setNomUtil(nomUtil);
-		mySopramon.setPrenomUtil(prenomUtil);
-		mySopramon.setUsername(username);
-		mySopramon.setPassword(password);
-		mySopramon.setDateNaissance(dateNaissance);
-		mySopramon.setExperience(0);
-		mySopramon.setNiveau(1);
-		mySopramon.setArgent(100.00d);
-		mySopramon.setNom(nom);
-		mySopramon.setSigne(SigneGenerator.getAstrologicalSign(dateNaissance));
-		daoSopramon.save(mySopramon);
-
-		return mySopramon;  
-	}
-
-	public static void addBoss(String nom, int niveau, int attaque, int defense, int pdv, int esquive, int vitesse, Date dateNaissance) {
-
-		IDAOBoss daoBoss = new DAOBossHibernate();
 		Boss myBoss = new Boss();
 
-		myBoss.setNom(nom);
-		myBoss.setNiveau(niveau);
-		myBoss.setSigne(SigneGenerator.getAstrologicalSign(dateNaissance));
-		myBoss.getCapacite().setAttaque(attaque);
-		myBoss.getCapacite().setDefense(defense);
-		myBoss.getCapacite().setPointsDeVie(pdv);
-		myBoss.getCapacite().setEsquive(esquive);
-		myBoss.getCapacite().setVitesse(vitesse);
+		myBoss.setNom(nomBoss);
+		myBoss.setNiveau(niveauBoss);
+		myBoss.setSigne(SigneGenerator.getAstrologicalSign(formatter.parse(jourNaissance + "/" + moisNaissance + "/" + anneeNaissance)));
+		myBoss.getCapacite().setAttaque(attaqueBoss);
+		myBoss.getCapacite().setDefense(defenseBoss);
+		myBoss.getCapacite().setPointsDeVie(pdvBoss);
+		myBoss.getCapacite().setEsquive(esquiveBoss);
+		myBoss.getCapacite().setVitesse(vitesseBoss);
 
 		daoBoss.save(myBoss);
+		DAOBossHibernate.close();
 	}
-	
-	public static void addItem(String nom, double prix, int attaque, int defense, int pdv, int esquive, int vitesse) {
 
-		IDAOItem daoItem = new DAOItemHibernate();
-		Item myItem = new Item();
-
-		myItem.setNom(nom);
-		myItem.setPrix(prix);
-		myItem.getCapacite().setAttaque(attaque);
-		myItem.getCapacite().setDefense(defense);
-		myItem.getCapacite().setPointsDeVie(pdv);
-		myItem.getCapacite().setEsquive(esquive);
-		myItem.getCapacite().setVitesse(vitesse);
-
-		daoItem.save(myItem);
-	}
-	
-	public static void modifyItem(String nom)  {
-		DAOItemHibernate daoItem = new DAOItemHibernate();
-		Item item = daoItem.findByNom(nom);	
-		Scanner keyboard = new Scanner(System.in);
-		
-	System.out.println("Quelle param�tre voulez-vous modifier ?\n"
-			+ "0 : le nom \n"
-			+ "1 : prix \n"
-			+"2 : PV\n"
-			+"3: Attaque\n"
-			+ "4 : Defense\n"
-			+ "5 : Esquive\n"
-			+ "6: Vitesse\n");
-	
-	int choix = keyboard.nextInt();
-
-	switch (choix)
-	{
-	  case 0:
-	    System.out.println("Entrer le nouveau nom");
-	    String nouveauNom = keyboard.next();
-	    item.setNom(nouveauNom);
-	    daoItem.save(item);
-	    break;
-	    
-	  case 1 :
-		  System.out.println("Entrer le nouveau prix");
-		    Double nouveauPrix = keyboard.nextDouble();
-		    item.setPrix(nouveauPrix);
-		    daoItem.save(item);
-	    break;
-	    
-	  case 2 :
-		  
-		 System.out.println("Entrer le nouveau PV");
-		    int nouveauPV = keyboard.nextInt();
-		    item.getCapacite().setPointsDeVie(nouveauPV);
-		    daoItem.save(item);
-	    break;
-	    
-	  case 3 :
-		  
-			 System.out.println("Entrer la nouvelle attaque");
-			    int nouveauAttaque = keyboard.nextInt();
-			    item.getCapacite().setAttaque(nouveauAttaque);
-			    daoItem.save(item);
-		    break;
-		    
-		    
-	  case 4 :
-		  
-			 System.out.println("Entrer la nouvelle d�fense");
-			    int nouveauDefense = keyboard.nextInt();
-			    item.getCapacite().setDefense(nouveauDefense);
-			    daoItem.save(item);
-		    break;
-		    
-	  case 5 :
-		  
-			 System.out.println("Entrer la nouvelle esquive");
-			    int nouvelleEsquive= keyboard.nextInt();
-			    item.getCapacite().setEsquive(nouvelleEsquive);
-			    daoItem.save(item);
-		    break;
-		    
-	  case 6 :
-		  
-			 System.out.println("Entrer la nouvelle vitesse");
-			    int nouvelleVitesse = keyboard.nextInt();
-			    item.getCapacite().setPointsDeVie(nouvelleVitesse);
-			    daoItem.save(item);
-		    break;
-	 
-	}
-	
-
-	double prixItem = keyboard.nextDouble();
-	System.out.println("Entrez l'attaque de l'item : ");
-	int attaqueItem = keyboard.nextInt();
-	System.out.println("Entrez la defense de l'item : ");
-	int defenseItem = keyboard.nextInt();
-	System.out.println("Entrez les pdv de l'item : ");
-	int pdvItem = keyboard.nextInt();
-	
-	keyboard.close();
-}
 }
--- sopramon-java/src/main/java/fr/sopra/main.java
+++ sopramon-java/src/main/java/fr/sopra/main.java
@@ -1,13 +1,10 @@
 package fr.sopra;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 import java.util.List;
 import java.util.Scanner;
-import fr.sopra.DAO.DAOBossSQL;
-import fr.sopra.DAO.DAOCombatSQL;
-import fr.sopra.DAO.DAOSopramonSQL;
+;
 import fr.sopra.DAOHibernate.DAOBossHibernate;
 import fr.sopra.DAOHibernate.DAOCombatHibernate;
 import fr.sopra.DAOHibernate.DAOItemHibernate;
@@ -15,139 +12,105 @@ import fr.sopra.DAOHibernate.DAOSopramonHibernate;
 import fr.sopra.DAOHibernate.ProgrammeGenerator;
 import fr.sopra.model.game.Boss;
 import fr.sopra.model.game.Combat;
+import fr.sopra.model.game.Item;
 import fr.sopra.model.game.Sopramon;
 
 public class main {
 
 	public static void main(String[] args) throws ParseException {
-		
-		ProgrammeGenerator.modifyItem("Cofee"); 
-		
 
 		Scanner keyboard = new Scanner(System.in);
+		int choix;
 
 		System.out.println("---------------------------------MENU---------------------------------------\n"
-				+ "1 : Créer un compte\n" + "2 : Se connecter\n" + "3 : Obtenir la liste de tous les Sopramon \n"
-				+ "4 : Démarrer un duel avec un Boss\n\n" + "Quel est votre choix ?\n");
+				+ "1 : Creer un compte\n" + "2 : Se connecter\n" + "3 : Obtenir la liste de tous les Sopramon \n"
+				+ "4 : Ajouter un item \n" +"5: Lister les items \n"+ "Quel est votre choix ?");
 
-
-
-		int choix = keyboard.nextInt();
-		String nomUtil;
-		String prenomUtil;
-		String nom;
-		String motDePasse;
 		String username;
-		String nomSopra;
-		int jourNaissance;
-		int moisNaissance;
-		int anneeNaissance;
-		
+		String motDePasse;
+		int choix1 = 0;
 		DAOSopramonHibernate daoSopramon = new DAOSopramonHibernate();
+		DAOItemHibernate daoItem = new DAOItemHibernate();
 		
+		switch (choix1) {
 		
-		switch (choix) {
 		
 		case 1:
 
-			System.out.println("-----Création du compte----");
-			System.out.println("Saisir votre prénom");
-			prenomUtil = keyboard.next();
-
-			System.out.println("Saisir votre nom :");
-			nomUtil = keyboard.next();
-
-			System.out.println("Saisir votre mot de passe :");
-			motDePasse = keyboard.next();
-
-			System.out.println("Entrez votre nom d'utilisateur");
-			username = keyboard.next();
-			
-			System.out.println("Entrez le nom de votre Sopramon");
-			nomSopra = keyboard.next();
-
-			 System.out.println("Veuillez saisir votre jour de naissance");
-			 jourNaissance = keyboard.nextInt();
-			 System.out.println("Veuillez saisir votre mois de naissance");
-			 moisNaissance = keyboard.nextInt();
-			 System.out.println("Veuillez saisir votre annee de naissance");
-			 anneeNaissance = keyboard.nextInt();
-
-			 System.out.println(jourNaissance+" "+moisNaissance+" "+anneeNaissance);
-			 
-			 SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-			 
-			 
-			Sopramon nouveauSopra = ProgrammeGenerator.addSopramon(nomUtil, prenomUtil, username, motDePasse, formatter.parse(jourNaissance + "/" + moisNaissance + "/" + anneeNaissance), nomSopra);
-	
-			System.out.println(nouveauSopra.toString());
-			
-			
+			Sopramon mySopramon = ProgrammeGenerator.addSopramon();
+			System.out.println(mySopramon.toString());
 			break;
-			
+
 		case 2:
+
 			System.out.println("Entrez votre nom d'utilisateur");
 			username = keyboard.next();
 			System.out.println("Entrez votre mot de passe");
 			motDePasse = keyboard.next();
+			Sopramon mySopramon1 = daoSopramon.seConnecter(username, motDePasse);
 
-			Sopramon mySopramon = daoSopramon.seConnecter(username, motDePasse);
-			
-			if (mySopramon != null) {
-			System.out.println("Connection avec votre Sopramon :");
-			System.out.println(mySopramon.toString());
-			}
-			else
+			if (mySopramon1 != null) {
+				System.out.println("Connection avec votre Sopramon :");
+				System.out.println(mySopramon1.toString());
+			} else
 				System.out.println("Echec de connection - veuillez saisir un mot de passe valide");
-
+			DAOSopramonHibernate.close();
 			break;
-			
-			
-			
 
 		case 3:
+			
+			
 			List<Sopramon> list = daoSopramon.findAll();
-			for (Sopramon p : list)
-				System.out.println(p.toString());
-
+			for (Sopramon s : list)
+				System.out.println(s.toString());
+			
 			break;
 
 		case 4:
-
-			System.out.println("Entrer le nom du Sopramon.");
-			String sop1 = keyboard.next();
 			
-			Sopramon sopra = daoSopramon.findByNom(sop1);
-			sopra=daoSopramon.findById(sopra.getId());
-			DAOBossHibernate daoBoss = new DAOBossHibernate();
-			DAOCombatHibernate daoCombat = new DAOCombatHibernate();
-			Boss nouveauBoss = daoBoss.findById(1);
-			System.out.println(sopra.toString());
-			System.out.println(nouveauBoss.toString());
-
-			if (sopra != null) {
-				Combat nouveauCombat = new Combat(sopra, nouveauBoss);
-				
+			ItemMenu.addItem();
+			break;
 			
-				daoCombat.saveBoss(nouveauCombat);
-				
-				System.out.println(nouveauCombat.toString());
-				
-				sopra.attaquerBoss(sopra, nouveauBoss, nouveauCombat);
-
-
-			} else {
-				System.out.println("Le Sopramon n'exsite pas.");
-			}
+		case 5:
+			
+			List<Item> listItem = daoItem.findAll();
+			for (Item i : listItem)
+				System.out.println(i.toString());
+			
 			break;
+//			System.out.println("Entrer le nom du Sopramon.");
+//			String sop1 = keyboard.next();
+//
+//			Sopramon sopra = daoSopramon.findByNom(sop1);
+//			sopra = daoSopramon.findById(sopra.getId());
+//			DAOBossHibernate daoBoss = new DAOBossHibernate();
+//			DAOCombatHibernate daoCombat = new DAOCombatHibernate();
+//			Boss nouveauBoss = daoBoss.findById(1);
+//			System.out.println(sopra.toString());
+//			System.out.println(nouveauBoss.toString());
+//
+//			if (sopra != null) {
+//				Combat nouveauCombat = new Combat(sopra, nouveauBoss);
+//
+//				daoCombat.saveBoss(nouveauCombat);
+//
+//				System.out.println(nouveauCombat.toString());
 
-		default:
+//				sopra.attaquerBoss(sopra, nouveauBoss, nouveauCombat);
 
-			System.out.println("Veuillez saisir une commande valide SVP.");
-			break;
+//			} else {
+//				System.out.println("Le Sopramon n'exsite pas.");
+//			}
+//			break;
+//
+//		default:
+//
+//			System.out.println("Veuillez saisir une commande valide SVP.");
+//			break;
 		}
-
-		}
+		DAOSopramonHibernate.close();
+		DAOItemHibernate.close();
+		keyboard.close();
+	}
 
 }
-
