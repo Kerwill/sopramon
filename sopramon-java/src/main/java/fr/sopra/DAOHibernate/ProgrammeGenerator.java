58d08152b4e935ce191838387b4938c619497bca
Unknown <samsamebe@gmail.com>
2018-08-03 09:55:08 +0200

    Item Boss add

--- sopramon-java/src/main/java/fr/sopra/DAOHibernate/ProgrammeGenerator.java
+++ sopramon-java/src/main/java/fr/sopra/DAOHibernate/ProgrammeGenerator.java
@@ -2,6 +2,7 @@ package fr.sopra.DAOHibernate;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Positive;
@@ -9,11 +10,13 @@ import javax.validation.constraints.PositiveOrZero;
 import javax.validation.constraints.Size;
 
 import fr.sopra.DAO.IDAOBoss;
+import fr.sopra.DAO.IDAOItem;
 import fr.sopra.DAO.IDAOSopramon;
 import fr.sopra.model.game.Achat;
 import fr.sopra.model.game.Boss;
 import fr.sopra.model.game.Capacite;
 import fr.sopra.model.game.Coup;
+import fr.sopra.model.game.Item;
 import fr.sopra.model.game.Signe;
 import fr.sopra.model.game.Sopramon;
 import fr.sopra.model.game.Type;
@@ -21,10 +24,48 @@ import fr.sopra.model.game.Type;
 public class ProgrammeGenerator {
 
 	public static void main(String[] args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+//créer un boss
+		System.out.println("-----Création d'un boss----");
+		System.out.println("Entrez le nom du boss : ");
+		String nomBoss = keyboard.next();
+		System.out.println("Entrez le niveau du boss : ");
+		int niveauBoss = keyboard.nextInt();
+		System.out.println("Entrez l'attaque du boss : ");
+		int attaqueBoss = keyboard.nextInt();
+		System.out.println("Entrez la defense du boss : ");
+		int defenseBoss = keyboard.nextInt();
+		System.out.println("Entrez les points de vie du boss : ");
+		int pdvBoss = keyboard.nextInt();
+		System.out.println("Entrez l'esquive du boss : ");
+		int esquiveBoss = keyboard.nextInt();
+		System.out.println("Entrez le nom de l'item : ");
+		int vitesseBoss = keyboard.nextInt();
+		
+		addBoss(nomBoss, niveauBoss, Signe.LION, attaqueBoss, defenseBoss, pdvBoss, esquiveBoss, vitesseBoss); // nom / niveau / signe / attaque / defense / pdv / esquive / vitesse
+		
+//créer un item		
+		System.out.println("-----Création d'un item ----");
+		System.out.println("Entrez le nom de l'item : ");
+		String nomItem = keyboard.next();
+		System.out.println("Entrez le nom de l'item : ");
+		double prixItem = keyboard.nextDouble();
+		System.out.println("Entrez le nom de l'item : ");
+		int attaqueItem = keyboard.nextInt();
+		System.out.println("Entrez le nom de l'item : ");
+		int defenseItem = keyboard.nextInt();
+		System.out.println("Entrez le nom de l'item : ");
+		int pdvItem = keyboard.nextInt();
+		System.out.println("Entrez le nom de l'item : ");
+		int esquiveItem = keyboard.nextInt();
+		System.out.println("Entrez le nom de l'item : ");
+		int vitesseItem = keyboard.nextInt();
+		
+		addItem(nomItem, prixItem, attaqueItem, defenseItem, pdvItem, esquiveItem, vitesseItem); // nom / prix / attaque / defense / pdv / esquive / vitesse
 
-		addBoss("Jeremy", 10, Signe.LION);
-		//
-
+		
 		DAOHibernate.close();
 	}
 
@@ -49,7 +90,7 @@ public class ProgrammeGenerator {
 		return mySopramon;
 	}
 
-	public static void addBoss(String nom, int niveau, Signe signe) {
+	public static void addBoss(String nom, int niveau, Signe signe, int attaque, int defense, int pdv, int esquive, int vitesse) {
 
 		IDAOBoss daoBoss = new DAOBossHibernate();
 		Boss myBoss = new Boss();
@@ -57,12 +98,28 @@ public class ProgrammeGenerator {
 		myBoss.setNom(nom);
 		myBoss.setNiveau(niveau);
 		myBoss.setSigne(signe);
-		myBoss.getCapacite().setAttaque(20);
-		myBoss.getCapacite().setDefense(5);
-		myBoss.getCapacite().setPointsDeVie(100);
-		myBoss.getCapacite().setEsquive(10);
-		myBoss.getCapacite().setVitesse(5);
+		myBoss.getCapacite().setAttaque(attaque);
+		myBoss.getCapacite().setDefense(defense);
+		myBoss.getCapacite().setPointsDeVie(pdv);
+		myBoss.getCapacite().setEsquive(esquive);
+		myBoss.getCapacite().setVitesse(vitesse);
 
 		daoBoss.save(myBoss);
+	}
+	
+	public static void addItem(String nom, double prix, int attaque, int defense, int pdv, int esquive, int vitesse) {
+
+		IDAOItem daoItem = new DAOItemHibernate();
+		Item myItem = new Item();
+
+		myItem.setNom(nom);
+		myItem.setPrix(prix);
+		myItem.getCapacite().setAttaque(attaque);
+		myItem.getCapacite().setDefense(defense);
+		myItem.getCapacite().setPointsDeVie(pdv);
+		myItem.getCapacite().setEsquive(esquive);
+		myItem.getCapacite().setVitesse(vitesse);
+
+		daoItem.save(myItem);
 	}
 }
