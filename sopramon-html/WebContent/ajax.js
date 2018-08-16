b58d07da7ed03aeedf457f14b34e850cdda81440
Unknown <aastruc86@gmail.com>
2018-08-16 15:01:58 +0200

    inscription + connexion

--- sopramon-html/WebContent/ajax.js
+++ sopramon-html/WebContent/ajax.js
@@ -1,72 +1,91 @@
-
 //inscription
-	var mySubmission = $('form #inscription');
-	mySubmission.bind('submit', function() {
+var mySubmission = $('#inscription');
+mySubmission.bind('submit', function() {
 
-		inscription();
-		return false;
-		
-		
-	});
+	inscription();
+	return false;
+
+});
+
 function inscription() {
 
-
-
-	
 	var myUser = {
-			username : $('form #inscription input[name = "username"]').val(),
-			prenom : $('form #inscription input[name = "firstname"]').val(),
-			nom : $('form #inscription input[name = "surname"]').val(),
-			password : $('form #inscription input[name = "password"]').val(),
-			dateNaissance: $('form #inscription input[name = "birthdate"]').val()
+		username : $('#inscription > input[name = "username"]').val(),
+		prenom : $('#inscription > input[name = "firstname"]').val(),
+		nom : $('#inscription >input[name = "surname"]').val(),
+		password : $('#inscription > input[name = "password"]').val(),
+		dateNaissance : $('#inscription >input[name = "birthdate"]').val()
 	}
-	
 
-	
-	
 	$.ajax({
 		method : 'POST',
 		url : 'http://192.168.1.100:8080/sopramon-web/api/sopramons',
 		// contentType : 'application/json',
 		// data : JSON.stringify(myUser),
-		data: myUser,
+		data : myUser,
 
 		success : function() {
-			
+
 			alert("Votre inscription a été validée")
 
 		}
 	});
 
-	};
+};
 
-	
 
-	
-	var myConnexion = $('form #connexion');
-	mySubmission.bind('submit', function() {
+//connexion 
 
-		connexion();
-		return false;
-	});
-	
-	
-	
-	function connexion() {
+
+
+
+var myConnexion = $("#connexion");
+myConnexion.bind('submit', function() {
+
+	connexion();
+	return false;
+});
+
+function connexion() {
 	
 	$.ajax({
-				method : 'POST',
-				url : 'http://192.168.1.100:8080/sopramon-web/api/auth',
-				data: {
-					username: $('form #connexion input[name = "username"]').val(),
-					password: $('form #connexion input[name = "password"]').val()
-					},
-				
-				success : function() {
-					alert("Connexion");
-				}
-			})};
+		method : 'POST',
+		url : 'http://192.168.1.100:8080/sopramon-web/api/auth',
+		data : {
+			username : $('#connexion > input[name = "username"]').val(),
+			password : $('#connexion > input[name = "password"]').val()
+		},
+
+		success : function(data) {
+			
+			alert("connexion");
+			
+			console.log(data);
+			appendSopramon(data);
+		}
+	})
+};
+
+
+function appendSopramon(sopramon) {
 	
+	let myRow = $('<tr />');
+	let myColNom = $('<td />');
+	let myColPrix = $('<td />');
+	let myColGamme = $('<td />');
+	
+	
+	myColNom.html(sopramon.username);
+	myColPrix.html(sopramon.signe);
+	myColGamme.html(sopramon.dateNaissance);
+	
+	myRow.append(myColNom);
+	myRow.append(myColPrix);
+	myRow.append(myColGamme);
 
 
-		
+	$('#sopramon tbody').append(myRow);
+
+}
+
+
