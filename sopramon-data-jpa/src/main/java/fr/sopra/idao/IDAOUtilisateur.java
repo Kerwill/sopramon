fc28ed20ebc48d008994161b601b83a0dab3649f
Unknown <aastruc86@gmail.com>
2018-08-31 12:47:07 +0200

    commit arnaud midi

--- sopramon-data-jpa/src/main/java/fr/sopra/idao/IDAOUtilisateur.java
+++ sopramon-data-jpa/src/main/java/fr/sopra/idao/IDAOUtilisateur.java
@@ -8,4 +8,5 @@ import fr.sopra.model.game.Sopramon;
 public interface IDAOUtilisateur extends JpaRepository<Sopramon, Integer>{
 
 	public Utilisateur findByUsernameAndPassword(String username, String password);
+
 }
