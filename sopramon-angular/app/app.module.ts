a583568705ebf4350582ff3a203feeec6c0f6543
Unknown <aastruc86@gmail.com>
2018-09-12 14:22:42 +0200

    commit

--- sopramon-angular/app/app.module.ts
+++ sopramon-angular/app/app.module.ts
@@ -11,7 +11,6 @@ import { CombatService } from './combat.service';
 import { ItemComponent } from './item.component';
 import { ItemService } from './item.service';
 import { AppConfigService} from './app-config.service';
-import { UtilisateurService } from './utilisateur.service';
 
 
 
@@ -34,7 +33,7 @@ const routes: Routes = [
       AchatService,
       CombatService,
       ItemService,
-      UtilisateurService
+
     ],
 
     declarations: [
